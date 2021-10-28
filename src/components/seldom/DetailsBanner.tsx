8624ebacc80d1c4bd8ceb8deae51ad7f7e33659e
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2021-10-28 08:53:35 +0700

    style(DetailsBanner): background overlay

--- src/components/seldom/DetailsBanner.tsx
+++ src/components/seldom/DetailsBanner.tsx
@@ -3,15 +3,18 @@ import Image from "@/components/shared/Image";
 
 const DetailsBanner = ({ image }: { image?: string }) => {
   return (
-    <div className="relative z-0 w-full h-[300px] before:absolute before:inset-0 before:bg-black/60 before:z-10">
+    <div className="relative z-0 w-full h-[400px]">
       {image && (
         <Image
           src={image}
           layout="fill"
           objectFit="cover"
+          objectPosition="50% 35%"
           alt="Details banner"
         />
       )}
+
+      <div className="banner__overlay absolute inset-0 z-10"></div>
     </div>
   );
 };
