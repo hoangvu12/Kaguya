2d28d1cce55d19dcfcd926eb8926f8340156e51c
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2021-11-04 15:34:26 +0700

    fix(Overlay): stop toggle video if mobile

--- src/components/shared/Video/Overlay.tsx
+++ src/components/shared/Video/Overlay.tsx
@@ -1,10 +1,14 @@
 import { useVideo } from "@/contexts/VideoContext";
+import useDevice from "@/hooks/useDevice";
 import React from "react";
 
 const Overlay: React.FC<React.HTMLProps<HTMLDivElement>> = (props) => {
   const { videoEl } = useVideo();
+  const { isMobile } = useDevice();
 
   const handleOverlayClick = () => {
+    if (!isMobile) return;
+
     if (videoEl.paused) {
       videoEl.play();
     } else {
