41d76f34f6f29cb8987f5e5c996ca13aa0f2a479
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2021-11-05 09:28:02 +0700

    fix(Head): change og:url to current url

--- src/components/shared/Head.tsx
+++ src/components/shared/Head.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NextHead from "next/head";
 import { WEBSITE_URL } from "@/constants";
+import { useRouter } from "next/router";
 
 interface HeadProps {
   title?: string;
@@ -15,6 +16,8 @@ const Head: React.FC<HeadProps> = (props) => {
     image = "https://i.ibb.co/JnDDN9j/localhost-3000-2.png",
   } = props;
 
+  const { asPath } = useRouter();
+
   return (
     <NextHead>
       <title>{title}</title>
@@ -22,13 +25,13 @@ const Head: React.FC<HeadProps> = (props) => {
       <meta name="description" content={description} />
 
       <meta property="og:type" content="website" />
-      <meta property="og:url" content={WEBSITE_URL} />
+      <meta property="og:url" content={WEBSITE_URL + asPath} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
 
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content={WEBSITE_URL} />
+      <meta property="twitter:url" content={WEBSITE_URL + asPath} />
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
       <meta property="twitter:image" content={image} />
