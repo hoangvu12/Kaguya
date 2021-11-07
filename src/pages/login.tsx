065ab99aabc766be1e90e06f6d25b92ecba45fd6
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2021-11-07 23:42:58 +0700

    fix(LoginPage): change styles and typo

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,14 +1,12 @@
 import Button from "@/components/shared/Button";
 import Head from "@/components/shared/Head";
-import Image from "@/components/shared/Image";
 import useSignIn from "@/hooks/useSignIn";
-import supabase from "@/lib/supabase";
 import quotes from "@/quotes.json";
 import { randomElement } from "@/utils";
 import { Provider } from "@supabase/gotrue-js";
 import axios from "axios";
-import { NextPage } from "next";
-import React from "react";
+import { GetStaticProps, NextPage } from "next";
+import React, { useMemo } from "react";
 import { FaFacebookF } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 
@@ -23,7 +21,7 @@ interface LoginPageProps {
 }
 
 const LoginPage: NextPage<LoginPageProps> = ({ quotes }) => {
-  const randomQuote = randomElement(quotes);
+  const randomQuote = useMemo(() => randomElement(quotes), [quotes]);
   const signInMutation = useSignIn({ redirectTo: "/" });
 
   const handleSignIn = (provider: Provider) => () => {
@@ -37,9 +35,9 @@ const LoginPage: NextPage<LoginPageProps> = ({ quotes }) => {
         description="Đăng nhập vào Kaguya để xem anime và sử dụng nhiều tính năng hay."
       />
 
-      <div className="w-full h-screen grid grid-cols-5">
+      <div className="w-full h-screen grid grid-cols-1 md:grid-cols-5">
         <div
-          className="relative col-span-2 px-16 py-8 after:absolute after:inset-0 after:bg-background/80 after:z-10"
+          className="hidden md:block relative col-span-2 after:absolute after:inset-0 after:bg-background/80 after:z-10"
           style={{
             backgroundImage: "url('/login-background.png')",
             backgroundPosition: "center center",
@@ -47,13 +45,9 @@ const LoginPage: NextPage<LoginPageProps> = ({ quotes }) => {
           }}
         >
           <div className="relative flex flex-col justify-center items-center w-full h-full z-20">
-            <div className="absolute left-1/2 -translate-x-1/2 top-5">
-              <Image src="/logo.png" alt="logo" width={100} height={100} />
-            </div>
-
-            <div className="w-full">
-              <h1 className="text-4xl font-semibold text-white">
-                "{randomQuote.quote}"
+            <div className="w-full px-8">
+              <h1 className="text-4xl font-semibold text-white line-clamp-6">
+                &quot;{randomQuote.quote}&quot;
               </h1>
               <p className="text-right text-xl italic mt-4 font-semibold">
                 {randomQuote.character}
@@ -65,7 +59,7 @@ const LoginPage: NextPage<LoginPageProps> = ({ quotes }) => {
           </div>
         </div>
         <div className="col-span-3 bg-background flex items-center justify-center">
-          <div className="w-1/2">
+          <div className="w-full px-4 md:px-0 md:w-1/2">
             <h1 className="text-5xl font-bold mb-8">Đăng nhập</h1>
 
             <Button
@@ -94,7 +88,7 @@ const LoginPage: NextPage<LoginPageProps> = ({ quotes }) => {
 // @ts-ignore
 LoginPage.getLayout = (page) => page;
 
-export const getStaticProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   try {
     const { data } = await axios.get("https://animechan.vercel.app/api/quotes");
 
@@ -102,6 +96,7 @@ export const getStaticProps = async () => {
       props: {
         quotes: data,
       },
+      revalidate: 86400,
     };
   } catch (err) {
     return {
