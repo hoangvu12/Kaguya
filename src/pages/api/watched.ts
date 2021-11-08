98881a276ccb64033b9d006888b15adfe980d90f
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2021-11-08 16:18:03 +0700

    fix(watchedAPI): change logic

--- src/pages/api/watched.ts
+++ src/pages/api/watched.ts
@@ -27,19 +27,45 @@ const handler: NextApiHandler = async (req, res) => {
       return;
     }
 
-    const { error } = await supabase
+    const { data: isWatched, error: watchedError } = await supabase
       .from("watched")
-      .upsert(
-        { user_id: user.id, anime_id, episode_id },
-        { onConflict: "anime_id" }
-      );
+      .select("id")
+      .eq("user_id", user.id)
+      .eq("anime_id", anime_id);
 
-    if (error) {
-      console.log(error);
-
-      res.json({ success: false, error: error.message });
+    if (watchedError) {
+      res.json({ success: false, error: watchedError.message });
 
       return;
+    }
+
+    if (isWatched?.length) {
+      const { error: updateError } = await supabase
+        .from("watched")
+        .update({
+          anime_id,
+          episode_id,
+        })
+        .match({ user_id: user.id, anime_id });
+
+      if (updateError) {
+        res.json({ success: false, error: updateError.message });
+
+        return;
+      }
+    } else {
+      const { error } = await supabase
+        .from("watched")
+        .upsert(
+          { user_id: user.id, anime_id, episode_id },
+          { ignoreDuplicates: false }
+        );
+
+      if (error) {
+        res.json({ success: false, error: error.message });
+
+        return;
+      }
     }
 
     res.json({ success: true });
