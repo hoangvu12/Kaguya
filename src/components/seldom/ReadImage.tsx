7994ea9d1a4a44efb3426219c7a83134422a86f5
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2021-11-28 22:48:57 +0700

    fix(ReadPage): images shift layout

--- src/components/seldom/ReadImage.tsx
+++ src/components/seldom/ReadImage.tsx
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+// @ts-ignore
+import imagesLoaded from "imagesloaded";
 
 interface ReadImageProps {
   images: string[];
+  onImagesLoaded?: () => void;
 }
 
-const ReadImage: React.FC<ReadImageProps> = ({ images }) => {
+const ReadImage: React.FC<ReadImageProps> = ({ images, onImagesLoaded }) => {
+  useEffect(() => {
+    imagesLoaded(".read-images", onImagesLoaded);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <div className="w-full">
+    <div className="w-full read-images">
       {images.map((image) => (
         // eslint-disable-next-line @next/next/no-img-element
         <img
           src={`/api/proxy?url=${image}`}
           alt="Đọc truyện tại Kaguya"
           key={image}
-          className="w-full"
+          className="w-full h-auto"
         />
       ))}
     </div>
