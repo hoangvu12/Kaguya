63d03ba5772130ace32ebc58d76581e3e6b114e2
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2021-12-04 07:36:51 +0700

    fix(ReadPage): change regex to parse number from chapter

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -136,5 +136,5 @@ export const getSeason = () => {
 
 // https://stackoverflow.com/questions/5457416/how-to-validate-numeric-values-which-may-contain-dots-or-commas
 export const parseNumbersFromString = (text: string) => {
-  return text.match(/\d{1,2}([\.,][\d{1,2}])?/g).map(Number);
+  return text.match(/\d+([\.,][\d{1,2}])?/g).map(Number);
 };
