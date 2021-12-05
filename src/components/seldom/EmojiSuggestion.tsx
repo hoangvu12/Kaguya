46a9ccc9011a6e30ac8132b2ed6b35113d320e92
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2021-12-05 22:43:34 +0700

    feat: Emoji suggestion

---
+++ src/components/seldom/EmojiSuggestion.tsx
@@ -0,0 +1,50 @@
+import { emojiSearch } from "@/utils/emoji";
+import classNames from "classnames";
+import { EmojiData } from "emoji-mart";
+import React, { useEffect } from "react";
+import Emoji from "../shared/Emoji";
+
+interface EmojiSuggestionProps
+  extends Omit<React.HTMLProps<HTMLDivElement>, "onClick"> {
+  text: string;
+  onClick?: (emoji: EmojiData) => void;
+}
+
+const EmojiSuggestion: React.FC<EmojiSuggestionProps> = ({
+  text,
+  className,
+  onClick,
+  ...props
+}) => {
+  const [emojis, setEmojis] = React.useState<EmojiData[]>(
+    emojiSearch(text).slice(0, 10)
+  );
+
+  const [show, setShow] = React.useState(true);
+
+  useEffect(() => {
+    setEmojis(emojiSearch(text).slice(0, 10).reverse());
+  }, [text]);
+
+  return !!emojis?.length && show ? (
+    <React.Fragment>
+      <div className={classNames("p-4", className)} {...props}>
+        {emojis.map((emoji) => (
+          <button
+            onClick={() => onClick(emoji)}
+            key={emoji.colons}
+            className="flex items-center w-full p-2 space-x-2 transition duration-300 rounded-md hover:bg-white/20"
+          >
+            <Emoji emoji={emoji.colons} size={20} />
+
+            <p className="text-base">{emoji.colons}</p>
+          </button>
+        ))}
+      </div>
+
+      <div className="fixed inset-0 z-40" onClick={() => setShow(false)}></div>
+    </React.Fragment>
+  ) : null;
+};
+
+export default React.memo(EmojiSuggestion);
--- src/components/shared/CommentInput.tsx
+++ src/components/shared/CommentInput.tsx
@@ -3,14 +3,16 @@ import EmojiPicker from "@/components/shared/EmojiPicker";
 import { useUser } from "@/contexts/AuthContext";
 import { customEmojis } from "@/utils/emoji";
 import { EmojiData } from "emoji-mart";
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { ContentEditableEvent } from "react-contenteditable";
+import EmojiSuggestion from "../seldom/EmojiSuggestion";
 import ClientOnly from "./ClientOnly";
 import EmojiText from "./EmojiText";
 
 const CommentInput = () => {
   const user = useUser();
   const [html, setHTML] = useState("");
+  const [latestText, setLatestText] = useState("");
 
   const handleChange = useCallback((event: ContentEditableEvent) => {
     setHTML(event.target.value);
@@ -23,6 +25,46 @@ const CommentInput = () => {
     [html]
   );
 
+  const handleEmojiSuggestionSelect = useCallback(
+    (emoji: EmojiData) => {
+      const index = html.lastIndexOf(latestText);
+      const replacedHTML =
+        html.substr(0, index) +
+        emoji.colons +
+        html.substr(index + latestText.length);
+
+      setHTML(replacedHTML);
+      setLatestText("");
+    },
+    [latestText, html]
+  );
+
+  const isShowEmojiSuggestion = latestText[0] === ":" && latestText.length > 2;
+
+  useEffect(() => {
+    const tempEl = document.createElement("div");
+
+    tempEl.innerHTML = html;
+
+    const latestNode = tempEl.childNodes[tempEl.childNodes.length - 1];
+
+    if (!latestNode) return;
+
+    if (latestNode.nodeName === "IMG") {
+      setLatestText("");
+
+      return;
+    }
+
+    setLatestText(
+      latestNode.textContent
+        .split(" ")
+        .slice(-1)[0]
+        .replace(/&nbsp;/g, " ")
+        .trim()
+    );
+  }, [html]);
+
   return (
     <ClientOnly>
       {user ? (
@@ -31,6 +73,14 @@ const CommentInput = () => {
 
           <div className="relative flex-1">
             <div className="relative bg-background-900">
+              {isShowEmojiSuggestion && (
+                <EmojiSuggestion
+                  text={latestText}
+                  className="absolute z-50 w-full bottom-full bg-background-900"
+                  onClick={handleEmojiSuggestionSelect}
+                />
+              )}
+
               {!html && (
                 <p className="absolute z-0 px-3 text-gray-400 -translate-y-1/2 top-1/2">
                   Nêu suy nghĩ của bạn...
