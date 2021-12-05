fe6f81bf561426f6bd170327fe9aec32d6441e21
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2021-12-05 19:05:47 +0700

    chore(EmojiUtils): custom emoji html

--- src/components/seldom/CommentsSection.tsx
+++ src/components/seldom/CommentsSection.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import CommentInput from "@/components/shared/CommentInput";
+import { Emoji } from "emoji-mart";
+import { NimbleEmojiIndex } from "emoji-mart";
+import { customEmojis } from "@/utils/emoji";
 
 const CommentsSection = () => {
   return (
     <div>
       <CommentInput />
+
+      <Emoji emoji={customEmojis[0]} size={24} />
     </div>
   );
 };
