3433880af11c87a61f68cdc66c251f046791407a
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2021-12-25 00:02:17 +0700

    chore: remove dayjs plugin

--- src/lib/dayjs.ts
+++ src/lib/dayjs.ts
@@ -1,11 +1,9 @@
 import dayjs from "dayjs";
 import updateLocale from "dayjs/plugin/updateLocale";
 import relativeTime from "dayjs/plugin/relativeTime";
-import weekDay from "dayjs/plugin/weekDay";
 
 dayjs.extend(updateLocale);
 dayjs.extend(relativeTime);
-dayjs.extend(weekDay);
 
 dayjs.updateLocale("en", {
   relativeTime: {
