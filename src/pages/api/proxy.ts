bf0088168ca1f0119607a21a3900949933381f48
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2021-12-27 13:36:07 +0700

    chore(source): use local proxy

--- src/pages/api/proxy.ts
+++ src/pages/api/proxy.ts
@@ -1,15 +1,38 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
+import config from "@/config";
+
+const getProxyUrl = (url: string) => {
+  return `/api/proxy?url=${encodeURIComponent(url)}`;
+};
 
 const proxy = async (req: NextApiRequest, res: NextApiResponse) => {
-  const url = decodeURIComponent(req.query.url as string);
+  let url = decodeURIComponent(req.query.url as string);
 
-  const response: any = await axios.get(url, {
+  const { host, ...headers } = req.headers;
+
+  const response = await axios.get<any, any, any>(url, {
     responseType: "stream",
+    // @ts-ignore
     headers: {
-      referer: "nettruyen",
+      ...headers,
+      referer: url.includes(".mp4") ? config.getSourceUrl : "nettruyen",
     },
+    maxRedirects: 0,
+    validateStatus: () => true,
   });
+
+  for (let header in response.headers) {
+    if (header.toLowerCase() === "location") {
+      res.redirect(getProxyUrl(response.headers[header]));
+
+      return;
+    }
+
+    res.setHeader(header, response.headers[header]);
+  }
+
+  res.status(response.status);
 
   response.data.pipe(res);
 };
