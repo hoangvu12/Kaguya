a0cfcac8be9bbb1d013dc143dd839cd3e5b1cebc
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2022-01-08 22:08:44 +0700

    feat: google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,26 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+// eslint-disable-next-line @next/next/no-script-in-document
+import Script from "next/script";
 
 class MyDocument extends Document {
   public render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-BG2PPGCZLW"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+         
+           gtag('config', 'G-BG2PPGCZLW');
+        `}
+          </Script>
+        </Head>
         <body>
           <Main />
           <NextScript />
