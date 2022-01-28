467ad966fa3587d88b7d36a5aba65b8f904d6aeb
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2022-01-28 14:28:25 +0700

    feat(CharacterConnectionCard): href to character page

--- src/components/shared/CharacterConnectionCard.tsx
+++ src/components/shared/CharacterConnectionCard.tsx
@@ -1,6 +1,7 @@
 import Image from "@/components/shared/Image";
 import { CharacterConnection } from "@/types";
 import { convert } from "@/utils/data";
+import Link from "next/link";
 import React from "react";
 
 interface CharacterCardProps<T> {
@@ -13,22 +14,28 @@ const CharacterConnectionCard = <T extends "anime" | "manga">({
   type: T,
 }: CharacterCardProps<T>) => {
   return (
-    <div className="text-gray-300 space-x-4 col-span-1 flex w-full h-24 bg-background-900">
-      <div className="relative h-full w-16">
-        <Image
-          src={characterConnection.character.image.large}
-          layout="fill"
-          objectFit="cover"
-          alt={`${characterConnection.character.name}`}
-        />
-      </div>
+    <Link href={`/characters/details/${characterConnection.character_id}`}>
+      <a>
+        <div className="text-gray-300 space-x-4 col-span-1 flex w-full h-24 bg-background-900 hover:bg-white/20 transtion duration-300">
+          <div className="relative h-full w-16">
+            <Image
+              src={characterConnection.character.image.large}
+              layout="fill"
+              objectFit="cover"
+              alt={`${characterConnection.character.name}`}
+            />
+          </div>
 
-      <div className="py-2 flex flex-col justify-between">
-        <p className="font-semibold">{characterConnection.character.name}</p>
+          <div className="py-2 flex flex-col justify-between">
+            <p className="font-semibold">
+              {characterConnection.character.name}
+            </p>
 
-        <p>{convert(characterConnection.role, "characterRole")}</p>
-      </div>
-    </div>
+            <p>{convert(characterConnection.role, "characterRole")}</p>
+          </div>
+        </div>
+      </a>
+    </Link>
   );
 };
 
