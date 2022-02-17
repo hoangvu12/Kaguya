edf148d0b33b4b2ff9e61dbaaa1f02eab4064599
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2022-02-17 11:11:22 +0700

    feat(useCharacterSearch): 🎸 sort based on favourites

--- src/hooks/useCharacterSearch.ts
+++ src/hooks/useCharacterSearch.ts
@@ -3,7 +3,11 @@ import { useSupaInfiniteQuery } from "@/utils/supabase";
 
 const useCharacterSearch = (keyword: string) => {
   return useSupaInfiniteQuery(["character", keyword], (from, to) =>
-    supabase.rpc("characters_search", { keyword }).select("*").range(from, to)
+    supabase
+      .rpc("characters_search", { keyword })
+      .select("*")
+      .range(from, to)
+      .order("favourites", { ascending: false })
   );
 };
 
