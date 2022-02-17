7bbb84a2167456d5e3134ced2d6f870ca578b948
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2022-02-17 11:10:12 +0700

    feat(VABrowsePage): 🎸 changes due to new data

--- src/hooks/useBirthdayVA.ts
+++ src/hooks/useBirthdayVA.ts
@@ -9,7 +9,7 @@ const useBirthdayVA = () => {
     ["voice-actors birthday"],
     () => {
       return supabase
-        .from("voice_actors")
+        .from("kaguya_voice_actors")
         .select("*")
         .eq("dateOfBirth->day", day.date())
         .eq("dateOfBirth->month", day.month() + 1)
--- src/hooks/useVASearch.ts
+++ src/hooks/useVASearch.ts
@@ -2,17 +2,15 @@ import supabase from "@/lib/supabase";
 import { useSupaInfiniteQuery } from "@/utils/supabase";
 
 const useVASearch = (keyword: string) => {
-  return useSupaInfiniteQuery(["va", keyword], (from, to) => {
-    const db = supabase
+  return useSupaInfiniteQuery(["va", keyword], (from, to) =>
+    supabase
       .rpc("voice_actors_search", {
         keyword,
       })
       .select("*")
-      .select("*")
-      .range(from, to);
-
-    return db;
-  });
+      .order("favourites", { ascending: false })
+      .range(from, to)
+  );
 };
 
 export default useVASearch;
