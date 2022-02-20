3905cdd13fb56826dcdad5c785834c7c622aaa53
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2022-02-20 11:04:14 +0700

    fix(useFetchImages): 🐛 add more params

--- src/hooks/useFetchImages.ts
+++ src/hooks/useFetchImages.ts
@@ -18,21 +18,27 @@ const useFetchImages = (currentChapter: Chapter, nextChapter?: Chapter) => {
 
   const fetchImages = (chapter: Chapter) =>
     axios
-      .get<ReturnSuccessType>(
-        `/api/manga/images?source_media_id=${chapter.sourceMediaId}&chapter_id=${chapter.sourceChapterId}&source_id=${chapter.sourceId}`
-      )
+      .get<ReturnSuccessType>(`/api/manga/images`, {
+        params: {
+          source_media_id: chapter.sourceMediaId,
+          chapter_id: chapter.sourceChapterId,
+          source_id: chapter.sourceId,
+        },
+      })
       .then(({ data }) => data);
 
+  const getQueryKey = (chapter: Chapter) =>
+    `images-${chapter.sourceId}-${chapter.sourceChapterId}`;
+
   return useQuery<ReturnSuccessType, AxiosError<ReturnFailType>>(
-    `images-${currentChapter.sourceChapterId}`,
+    getQueryKey(currentChapter),
     () => fetchImages(currentChapter),
     {
       onSuccess: () => {
         if (!nextChapter?.sourceChapterId) return;
 
-        queryClient.prefetchQuery(
-          `images-${nextChapter?.sourceChapterId}`,
-          () => fetchImages(nextChapter)
+        queryClient.prefetchQuery(getQueryKey(nextChapter), () =>
+          fetchImages(nextChapter)
         );
       },
     }
