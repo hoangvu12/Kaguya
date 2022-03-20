90da04b9e56ac3e2047a1b50deb4b7c1e22332ab
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2022-03-20 20:33:37 +0700

    feat(ReadPage): 🎸 click navigator

--- src/components/features/manga/Reader/ImageNavigator.tsx
+++ src/components/features/manga/Reader/ImageNavigator.tsx
@@ -1,22 +1,11 @@
-import Button from "@/components/shared/Button";
 import { useReadContainer } from "@/contexts/ReadContainerContext";
 import { useReadInfo } from "@/contexts/ReadContext";
-import { useReadPanel } from "@/contexts/ReadPanelContext";
 import { useReadSettings } from "@/contexts/ReadSettingsContext";
-import useDevice from "@/hooks/useDevice";
-import classNames from "classnames";
-import { AnimatePresence, motion } from "framer-motion";
 import React, { useCallback, useEffect } from "react";
-import {
-  HiOutlineArrowNarrowLeft,
-  HiOutlineArrowNarrowRight,
-} from "react-icons/hi";
+import ButtonNavigator from "./ButtonNavigator";
+import ClickNavigator from "./ClickNavigator";
 
 const ImageNavigator = () => {
-  const { isMobile } = useDevice();
-  const {
-    state: { isSidebarOpen },
-  } = useReadPanel();
   const {
     state: { activeImageIndex },
     setState,
@@ -79,47 +68,10 @@ const ImageNavigator = () => {
   }, [handleLeft, handleRight]);
 
   return (
-    <AnimatePresence initial={!isMobile}>
-      {direction !== "vertical" && (!isMobile || isSidebarOpen) && (
-        <motion.div
-          animate={{ opacity: !isMobile ? 0 : 1 }}
-          whileHover={{ opacity: 1 }}
-          initial={{ opacity: 0 }}
-          exit={{ opacity: 0 }}
-          className={classNames(
-            "w-11/12 md:w-4/6 z-50 flex items-center justify-between bottom-5 rounded-md p-2 bg-background-800 fixed"
-          )}
-        >
-          <Button
-            secondary
-            LeftIcon={HiOutlineArrowNarrowLeft}
-            onClick={direction === "ltr" ? previousImage : nextImage}
-          >
-            {isMobile ? null : direction === "ltr" ? (
-              <p>Ảnh trước</p>
-            ) : (
-              <p>Ảnh tiếp theo</p>
-            )}
-          </Button>
-
-          <p>
-            {activeImageIndex + 1} / {images.length}
-          </p>
-
-          <Button
-            secondary
-            RightIcon={HiOutlineArrowNarrowRight}
-            onClick={direction === "ltr" ? nextImage : previousImage}
-          >
-            {isMobile ? null : direction === "ltr" ? (
-              <p>Ảnh tiếp theo</p>
-            ) : (
-              <p>Ảnh trước</p>
-            )}
-          </Button>
-        </motion.div>
-      )}
-    </AnimatePresence>
+    <React.Fragment>
+      <ClickNavigator onLeft={handleLeft} onRight={handleRight} />
+      <ButtonNavigator onLeft={handleLeft} onRight={handleRight} />
+    </React.Fragment>
   );
 };
 
