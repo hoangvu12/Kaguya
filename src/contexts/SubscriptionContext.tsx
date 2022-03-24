2a29195260b642938bafea8589324b559e731183
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2022-03-24 10:15:30 +0700

    fix(SubscriptionContext): 🐛 convert public key to Uint8Array

--- src/contexts/SubscriptionContext.tsx
+++ src/contexts/SubscriptionContext.tsx
@@ -1,5 +1,6 @@
 import config from "@/config";
 import useCreateSubscription from "@/hooks/useCreateSubscription";
+import { urlBase64ToUint8Array } from "@/utils";
 import {
   createContext,
   Dispatch,
@@ -33,7 +34,7 @@ export const SubscriptionContextProvider: React.FC = ({ children }) => {
       if (!subscription) {
         subscription = await registration.pushManager.subscribe({
           userVisibleOnly: true,
-          applicationServerKey: config.webPushPublicKey,
+          applicationServerKey: urlBase64ToUint8Array(config.webPushPublicKey),
         });
       }
 
