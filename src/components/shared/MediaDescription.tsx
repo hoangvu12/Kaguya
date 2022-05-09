758b6193e309f05f333d83345c9c530d2e891731
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2022-05-09 07:48:54 +0700

    feat(MediaDescription): 🎸 responsive for mobile devices

--- src/components/shared/MediaDescription.tsx
+++ src/components/shared/MediaDescription.tsx
@@ -1,3 +1,4 @@
+import useDevice from "@/hooks/useDevice";
 import classNames from "classnames";
 import { useTranslation } from "next-i18next";
 import React, { useCallback, useEffect, useRef } from "react";
@@ -6,6 +7,8 @@ import Description, { DescriptionProps } from "./Description";
 interface MediaDescriptionProps extends DescriptionProps {
   containerClassName?: string;
 }
+
+const noop = () => {};
 
 const MediaDescription: React.FC<MediaDescriptionProps> = ({
   description,
@@ -17,6 +20,7 @@ const MediaDescription: React.FC<MediaDescriptionProps> = ({
     React.useState(false);
   const { t } = useTranslation("common");
   const ref = useRef<HTMLDivElement>(null);
+  const { isMobile } = useDevice();
 
   useEffect(() => {
     if (!ref.current) return;
@@ -43,10 +47,11 @@ const MediaDescription: React.FC<MediaDescriptionProps> = ({
           isDescriptionExpanded ? "line-clamp-none" : "line-clamp-6",
           className
         )}
+        onClick={isMobile ? handleClick : noop}
         {...props}
       />
 
-      {!isDescriptionExpanded && (
+      {!isDescriptionExpanded && !isMobile && (
         <button
           onClick={handleClick}
           className="bg-gradient-to-t from-background-900 via-background-900/80 to-transparent opacity-0 group-hover:opacity-100 transition duration-300 absolute bottom-0 w-full h-12 text-center"
