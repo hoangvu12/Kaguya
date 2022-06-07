80488ed425cdeef03b28eaa186f849ead048e5f8
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2022-06-07 12:54:20 +0700

    fix: 🐛 changes due to netplayer's api changes

--- src/components/features/anime/Player/Subtitle.tsx
+++ src/components/features/anime/Player/Subtitle.tsx
@@ -58,8 +58,7 @@ const Subtitle = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   const subtitle = useMemo(
-    () =>
-      state.subtitles?.find((sub) => sub.lang === state.currentSubtitle?.lang),
+    () => state.subtitles?.find((sub) => sub.lang === state.currentSubtitle),
     [state.subtitles, state.currentSubtitle]
   );
 
