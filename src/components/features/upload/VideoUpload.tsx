5fa02dcfe8aff3c556419e3aff890c7c9b206b70
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2022-07-03 16:53:26 +0700

    feat(VideoUpload): 🎸 preview video before uploading

--- src/components/features/upload/VideoUpload.tsx
+++ src/components/features/upload/VideoUpload.tsx
@@ -1,6 +1,8 @@
-import FileUploading from "@/components/shared/FileUploading";
+import Button from "@/components/shared/Button";
+import FileUploading, { FileUploader } from "@/components/shared/FileUploading";
 import { supportedUploadVideoFormats } from "@/constants";
 import { useRef } from "react";
+import { AiOutlineDelete, AiOutlineEdit } from "react-icons/ai";
 import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 
 interface VideoUploadProps {
@@ -29,7 +31,42 @@ const VideoUpload: React.FC<VideoUploadProps> = ({ onChange }) => {
         <FileUploading
           onChange={handleFileChange}
           acceptType={supportedUploadVideoFormats}
-        />
+        >
+          {(props) => {
+            const file = props.fileList[0];
+
+            if (!file) {
+              return <FileUploader {...props} />;
+            }
+
+            const src = URL.createObjectURL(file);
+
+            return (
+              <div className="relative">
+                <div className="relative w-full flex items-center aspect-w-16 aspect-h-9">
+                  <video controls src={src} className="object-contain" />
+                </div>
+                <div className="bg-background-800 absolute -top-2 -right-2 flex items-center">
+                  <Button
+                    secondary
+                    onClick={() => props.onFileUpdate(0)}
+                    LeftIcon={AiOutlineEdit}
+                    iconClassName="w-6 h-6"
+                    className="!p-1"
+                  />
+
+                  <Button
+                    secondary
+                    onClick={() => props.onFileRemove(0)}
+                    LeftIcon={AiOutlineDelete}
+                    iconClassName="text-red-500 w-6 h-6"
+                    className="!p-1"
+                  />
+                </div>
+              </div>
+            );
+          }}
+        </FileUploading>
       </TabPanel>
       <TabPanel>
         <textarea
