ad5ca0affdf1c9f05c67e53495114609968b2190
hoangvu12 <hggaming91@gmail.com>
2022-07-06 12:48:53 +0700

    fix(entry): 🐛 put sentry dsn to enviroment

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -7,9 +7,7 @@ import * as Sentry from "@sentry/nextjs";
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn:
-    SENTRY_DSN ||
-    "https://0c6cedcad06f48a88d3e6fa27da3d303@o1277462.ingest.sentry.io/6474953",
+  dsn: SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
 
