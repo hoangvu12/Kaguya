14150a2f82a3dffa2ce78eb13386441a393ee8b8
hoangvu12 <hggaming91@gmail.com>
2022-07-06 12:47:41 +0700

    fix(entry): 🐛 disable on development

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -2,14 +2,19 @@
 // The config you add here will be used whenever a page is visited.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://0c6cedcad06f48a88d3e6fa27da3d303@o1277462.ingest.sentry.io/6474953',
+  dsn:
+    SENTRY_DSN ||
+    "https://0c6cedcad06f48a88d3e6fa27da3d303@o1277462.ingest.sentry.io/6474953",
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
+
+  enabled: process.env.NODE_ENV === "production",
+
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
