fcc40dd6b258c2212a74e041fcd913b6bed7e36f
nknodev <52920235+nknodev@users.noreply.github.com>
2022-07-13 17:37:54 +0300

    Russian Translations Add (#12)
    * uwu
    
    * Update dayjs.ts
    
    * Update AnimeScheduling.tsx
    
    * Update locales.json
    
    * complete translate

--- src/lib/dayjs.ts
+++ src/lib/dayjs.ts
@@ -2,6 +2,7 @@ import dayjs from "dayjs";
 import updateLocale from "dayjs/plugin/updateLocale";
 import relativeTime from "dayjs/plugin/relativeTime";
 import "dayjs/locale/vi";
+import "dayjs/locale/ru";
 
 dayjs.extend(updateLocale);
 dayjs.extend(relativeTime);
