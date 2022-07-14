e9b98633c0d6326d6bd89216044e45cbe033447b
hoangvu12 <hggaming91@gmail.com>
2022-07-14 17:07:49 +0700

    fix(VideoUpload): 🐛 remove file upload

--- src/components/features/upload/VideoUpload.tsx
+++ src/components/features/upload/VideoUpload.tsx
@@ -1,12 +1,7 @@
-import Button from "@/components/shared/Button";
-import FileUploading, { FileUploader } from "@/components/shared/FileUploading";
 import Loading from "@/components/shared/Loading";
 import Select from "@/components/shared/Select";
-import { supportedUploadVideoFormats } from "@/constants";
 import useHostings from "@/hooks/useHostings";
-import { useMemo, useRef, useState } from "react";
-import { AiOutlineDelete, AiOutlineEdit } from "react-icons/ai";
-import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+import React, { useMemo, useRef, useState } from "react";
 
 export type VideoState = {
   video: File | string;
@@ -26,10 +21,6 @@ const VideoUpload: React.FC<VideoUploadProps> = ({ onChange }) => {
 
   const [hostingId, setHostingId] = useState<string>(null);
 
-  const handleFileChange = (fileList: File[]) => {
-    onChange?.({ video: fileList[0], hostingId });
-  };
-
   const handleTextAreaBlur = () => {
     onChange?.({ video: textAreaRef.current.value, hostingId });
   };
@@ -42,6 +33,8 @@ const VideoUpload: React.FC<VideoUploadProps> = ({ onChange }) => {
 
   const handleSelectChange = ({ value }: { value: string }) => {
     setHostingId(value);
+
+    onChange?.({ video: textAreaRef.current.value, hostingId });
   };
 
   return (
@@ -49,13 +42,10 @@ const VideoUpload: React.FC<VideoUploadProps> = ({ onChange }) => {
       {isLoading ? (
         <Loading />
       ) : (
-        <Tabs selectedTabClassName="bg-primary-500 rounded-md">
-          <div className="flex justify-between items-center gap-2 mb-4">
-            <TabList className="flex items-center gap-2">
-              <Tab className="cursor-pointer p-2">Upload bằng file</Tab>
-              <Tab className="cursor-pointer p-2">Upload bằng url</Tab>
-            </TabList>
+        // Remove file upload because the server can't handle large files
 
+        <React.Fragment>
+          <div className="flex justify-end">
             <Select
               options={selectValue}
               placeholder="Hosting"
@@ -63,56 +53,13 @@ const VideoUpload: React.FC<VideoUploadProps> = ({ onChange }) => {
             />
           </div>
 
-          <TabPanel>
-            <FileUploading
-              onChange={handleFileChange}
-              acceptType={supportedUploadVideoFormats}
-            >
-              {(props) => {
-                const file = props.fileList[0];
-
-                if (!file) {
-                  return <FileUploader {...props} />;
-                }
-
-                const src = URL.createObjectURL(file);
-
-                return (
-                  <div className="relative">
-                    <div className="relative w-full flex items-center aspect-w-16 aspect-h-9">
-                      <video controls src={src} className="object-contain" />
-                    </div>
-                    <div className="bg-background-800 absolute -top-2 -right-2 flex items-center">
-                      <Button
-                        secondary
-                        onClick={() => props.onFileUpdate(0)}
-                        LeftIcon={AiOutlineEdit}
-                        iconClassName="w-6 h-6"
-                        className="!p-1"
-                      />
-
-                      <Button
-                        secondary
-                        onClick={() => props.onFileRemove(0)}
-                        LeftIcon={AiOutlineDelete}
-                        iconClassName="text-red-500 w-6 h-6"
-                        className="!p-1"
-                      />
-                    </div>
-                  </div>
-                );
-              }}
-            </FileUploading>
-          </TabPanel>
-          <TabPanel>
-            <textarea
-              ref={textAreaRef}
-              onBlur={handleTextAreaBlur}
-              className="mt-2 p-2 w-full h-36 bg-background-900 text-white border-gray-300 border"
-              placeholder="Nhập URL muốn upload (VD: https://example.com/video.mp4)"
-            />
-          </TabPanel>
-        </Tabs>
+          <textarea
+            ref={textAreaRef}
+            onBlur={handleTextAreaBlur}
+            className="mt-2 p-2 w-full h-36 bg-background-900 text-white border-gray-300 border"
+            placeholder="Nhập URL muốn upload (VD: https://example.com/video.mp4)"
+          />
+        </React.Fragment>
       )}
     </div>
   );
