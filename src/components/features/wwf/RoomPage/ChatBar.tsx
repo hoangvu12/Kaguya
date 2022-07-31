06ce0b2bc42d6df7a6df1c296268f5abc498229f
hoangvu12 <hggaming91@gmail.com>
2022-07-31 10:49:41 +0700

    feat(ChatBar): 🎸 notification sound on message receive

--- src/components/features/wwf/RoomPage/ChatBar.tsx
+++ src/components/features/wwf/RoomPage/ChatBar.tsx
@@ -8,12 +8,23 @@ import Chat from "./Chat";
 const ChatBar = () => {
   const { socket, roomUser } = useRoomInfo();
   const [chats, setChats] = useState<ChatType[]>([]);
-  const messageBottomRef = useRef<HTMLDivElement>();
   const [message, setMessage] = useState("");
+  const messageBottomRef = useRef<HTMLDivElement>();
+  const audioRef = useRef<HTMLAudioElement>();
 
   useEffect(() => {
     const handleMessage = (message: ChatMessage) => {
       setChats((prev) => [...prev, { ...message, type: "message" }]);
+
+      if (audioRef.current) {
+        audioRef.current.pause();
+
+        audioRef.current.volume = 0.2;
+
+        audioRef.current.currentTime = 0;
+
+        audioRef.current.play();
+      }
 
       messageBottomRef.current?.scrollIntoView();
     };
@@ -63,28 +74,35 @@ const ChatBar = () => {
   }, [handleSendMessage, message]);
 
   return (
-    <div className="relative flex flex-col h-full">
-      <div className="grow pb-4 space-y-2 overflow-y-auto no-scrollbar">
-        {chats.map((chat, index) => (
-          <Chat chat={chat} key={index} />
-        ))}
+    <React.Fragment>
+      <audio ref={audioRef} src="/sounds/notification.mp3">
+        Your browser does not support the
+        <code>audio</code> element.
+      </audio>
 
-        <div ref={messageBottomRef}></div>
+      <div className="relative flex h-full flex-col">
+        <div className="no-scrollbar grow space-y-2 overflow-y-auto pb-4">
+          {chats.map((chat, index) => (
+            <Chat chat={chat} key={index} />
+          ))}
+
+          <div ref={messageBottomRef}></div>
+        </div>
+
+        <div className="sticky bottom-0 w-full bg-background-900">
+          <Input
+            value={message}
+            placeholder="Aa"
+            className="bg-background-800 px-3 py-2"
+            onChange={(e) => {
+              const target = e.target as HTMLInputElement;
+
+              setMessage(target.value);
+            }}
+          />
+        </div>
       </div>
-
-      <div className="sticky bottom-0 w-full bg-background-900">
-        <Input
-          value={message}
-          placeholder="Aa"
-          className="px-3 py-2 bg-background-800"
-          onChange={(e) => {
-            const target = e.target as HTMLInputElement;
-
-            setMessage(target.value);
-          }}
-        />
-      </div>
-    </div>
+    </React.Fragment>
   );
 };
 
