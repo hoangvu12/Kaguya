6a277da30c5edd3bf59309ea02db7e3f2b7f01c9
hoangvu12 <hggaming91@gmail.com>
2022-08-12 17:41:04 +0700

    feat(CardSwiper): 🎸 rewrite animation

--- src/components/shared/CardSwiper.tsx
+++ src/components/shared/CardSwiper.tsx
@@ -1,20 +1,205 @@
-import Card from "@/components/shared/Card";
-import Swiper, { SwiperSlide } from "@/components/shared/Swiper";
+import SwiperCard from "@/components/shared/SwiperCard";
+import Swiper, {
+  SwiperInstance,
+  SwiperSlide,
+} from "@/components/shared/Swiper";
 import { Media } from "@/types/anilist";
-import React from "react";
+import { debounce } from "@/utils";
+import React, { useState } from "react";
+import { isMobile } from "react-device-detect";
 
 interface CardSwiperProps {
   data: Media[];
-  onEachCard?: (data: Media) => React.ReactNode;
+  onEachCard?: (data: Media, isHover: boolean) => React.ReactNode;
 }
 
+// Will take 3 times norma; card's width on hover
+const HOVER_WIDTH = 3;
+
+const noop = () => {};
+
+const getVisibleIndex = (swiper: SwiperInstance) => {
+  const { slides } = swiper;
+
+  const visibleCards = slides
+    .map((slide, index) => ({ slide, index }))
+    .filter(({ slide }) => slide.classList.contains("swiper-slide-visible"));
+
+  return {
+    first: visibleCards[0].index,
+    last: visibleCards[visibleCards.length - 1].index,
+  };
+};
+
 const CardSwiper: React.FC<CardSwiperProps> = (props) => {
-  const { data, onEachCard = (data) => <Card data={data} /> } = props;
+  const {
+    data,
+    onEachCard = (data, isHover) => (
+      <SwiperCard isExpanded={isHover} data={data} />
+    ),
+  } = props;
+
+  const [swiper, setSwiper] = useState<SwiperInstance>();
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  // I'm sorry for those who are reading this code
+  // Even myself don't know why it works.
+  // Please don't touch it
+
+  const handleSlideHover = (index: number) => () => {
+    if (!swiper) return;
+
+    const currentSlide = swiper.slides[index] as HTMLElement;
+    const nextSlide = swiper.slides[index + 1] as HTMLElement;
+
+    currentSlide.classList.add("swiper-animating");
+
+    // @ts-ignore
+    const [originalWidth] = swiper.slidesSizesGrid as number[];
+
+    let spaceBetween = 0;
+    let slidesPerGroup = 1;
+
+    const currentBreakpoint =
+      swiper.params.breakpoints[swiper.currentBreakpoint];
+
+    spaceBetween = currentBreakpoint.spaceBetween || swiper.params.spaceBetween;
+    slidesPerGroup =
+      currentBreakpoint.slidesPerGroup || swiper.params.slidesPerGroup;
+
+    const isVisible = currentSlide.classList.contains("swiper-slide-visible");
+
+    currentSlide.style.transition = "all 300ms";
+
+    if (!isVisible) return;
+
+    const { first: firstVisibleCardIndex, last: lastVisibleCardIndex } =
+      getVisibleIndex(swiper);
+
+    const shouldPushSlide =
+      lastVisibleCardIndex - (HOVER_WIDTH - 1) < index ||
+      lastVisibleCardIndex === index;
+
+    if (shouldPushSlide) {
+      if (!nextSlide) {
+        // Add a placeholder slide to push current slide to the left
+        const element = document.createElement("div");
+        element.className = "swiper-slide swiper-placeholder";
+
+        swiper.$wrapperEl[0].append(element);
+
+        swiper.updateSlides();
+      }
+
+      const newTranslate =
+        -1 *
+        ((firstVisibleCardIndex + HOVER_WIDTH - 1) *
+          (originalWidth + spaceBetween));
+
+      // @ts-ignore
+      swiper.setTransition(300);
+      swiper.setTranslate(newTranslate);
+    }
+
+    setActiveIndex(index);
+
+    currentSlide.style.width = `${
+      originalWidth * HOVER_WIDTH - 1 + spaceBetween
+    }px`;
+  };
+
+  const handleSlideLeave = (index: number) => () => {
+    if (!swiper) return;
+
+    const currentSlide = swiper.slides[index] as HTMLElement;
+    const nextSlide = swiper.slides[index + 1] as HTMLElement;
+
+    // @ts-ignore
+    const [originalWidth] = swiper.slidesSizesGrid as number[];
+
+    currentSlide.style.width = `${originalWidth}px`;
+
+    let spaceBetween = 0;
+    let slidesPerGroup = 1;
+
+    const currentBreakpoint =
+      swiper.originalParams.breakpoints[swiper.currentBreakpoint];
+
+    spaceBetween =
+      currentBreakpoint.spaceBetween || swiper.originalParams.spaceBetween;
+    slidesPerGroup =
+      currentBreakpoint.slidesPerGroup || swiper.originalParams.slidesPerGroup;
+
+    const isAnimating = currentSlide.classList.contains("swiper-animating");
+
+    if (!isAnimating) return;
+
+    const { first: firstVisibleCardIndex } = getVisibleIndex(swiper);
+
+    const revertTranslate = () => {
+      const newTranslate =
+        -1 *
+        ((firstVisibleCardIndex - (HOVER_WIDTH - 1)) *
+          (originalWidth + spaceBetween));
+
+      // @ts-ignore
+      swiper.setTransition(300);
+      swiper.setTranslate(newTranslate);
+    };
+
+    const nonPlaceholderSlides = swiper.slides.filter(
+      (slide) => !slide.classList.contains("swiper-placeholder")
+    );
+
+    if (
+      index ===
+        // @ts-ignore
+        slidesPerGroup * (swiper.snapIndex + 1) - 1 ||
+      index >=
+        // @ts-ignore
+        slidesPerGroup * (swiper.snapIndex + 1) - (HOVER_WIDTH - 1)
+    ) {
+      revertTranslate();
+    } else if (index > slidesPerGroup) {
+      // Remove by two because of placeholder card
+      if (
+        index === nonPlaceholderSlides.length - 1 ||
+        index >= nonPlaceholderSlides.length - (HOVER_WIDTH - 1)
+      ) {
+        revertTranslate();
+      }
+    }
+
+    currentSlide.classList.remove("swiper-animating");
+
+    if (nextSlide?.classList.contains("swiper-placeholder")) {
+      swiper.slides.eq(swiper.slides.length - 1).remove();
+      swiper.update();
+    }
+
+    setActiveIndex(null);
+  };
 
   return (
-    <Swiper speed={500}>
+    <Swiper
+      onSwiper={(swiper) => {
+        setSwiper(swiper);
+      }}
+      speed={500}
+      watchSlidesVisibility
+    >
       {data.map((item, index) => (
-        <SwiperSlide key={index}>{onEachCard(item)}</SwiperSlide>
+        <SwiperSlide
+          onMouseEnter={
+            isMobile ? noop : debounce(handleSlideHover(index), 300)
+          }
+          onMouseLeave={
+            isMobile ? noop : debounce(handleSlideLeave(index), 300)
+          }
+          key={index}
+        >
+          {onEachCard(item, activeIndex === index)}
+        </SwiperSlide>
       ))}
     </Swiper>
   );
