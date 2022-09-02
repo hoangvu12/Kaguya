819d6484b279e1e67a84789164d4b9e25570e732
hoangvu12 <hggaming91@gmail.com>
2022-09-03 00:30:46 +0700

    feat(ScreenshotButton): 🎸 support multiple layouts

--- src/components/features/anime/Player/ScreenshotButton.tsx
+++ src/components/features/anime/Player/ScreenshotButton.tsx
@@ -1,6 +1,12 @@
 import CircleButton from "@/components/shared/CircleButton";
 import Modal from "@/components/shared/Modal";
-import { array_move, download, randomString } from "@/utils";
+import {
+  array_move,
+  download,
+  drawImageProp,
+  parseTime,
+  randomString,
+} from "@/utils";
 import {
   BackwardButton,
   ControlButton,
@@ -9,21 +15,155 @@ import {
   useVideo,
   useVideoProps,
 } from "netplayer";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { AiOutlineCamera, AiOutlineSave } from "react-icons/ai";
 import { BiDownArrowAlt, BiUpArrowAlt } from "react-icons/bi";
 import { CgClose } from "react-icons/cg";
+import { MdOutlinePreview } from "react-icons/md";
 import { toast } from "react-toastify";
 import ProgressSlider from "./ProgressSlider";
+
+type ImagePosition = [number, number]; // [column, row]
+
+type ImageLayout = {
+  positions: {
+    grid: ImagePosition;
+    startColumn: number;
+    startRow: number;
+  }[];
+  canvasWidth?: (defaultWidth: number) => number;
+  canvasHeight?: (defaultHeight: number) => number;
+};
+
+type ScreenshotImage = {
+  imageUrl: string;
+  screenshotTime: number;
+};
 
 const IMAGE_WIDTH = 1920;
 const IMAGE_HEIGHT = 1080;
 
+const CANVAS_COLUMNS = 12;
+const CANVAS_ROWS = 12;
+
+const MAX_IMAGES = 4;
+
+const LAYOUTS: ImageLayout[] = [
+  // =============
+  // ||    1    ||
+  // =============
+  // ||    2    ||
+  // =============
+  {
+    positions: [
+      {
+        grid: [CANVAS_COLUMNS, CANVAS_ROWS / 2],
+        startColumn: 0,
+        startRow: 0,
+      },
+      {
+        grid: [CANVAS_COLUMNS, CANVAS_ROWS / 2],
+        startColumn: 0,
+        startRow: CANVAS_COLUMNS / 2,
+      },
+    ],
+    canvasHeight: (defaultHeight: number) => defaultHeight * 2,
+  },
+
+  // =============
+  // ||    1    ||
+  // =============
+  // ||    2    ||
+  // =============
+  // ||    3    ||
+  // =============
+  {
+    positions: [
+      {
+        grid: [CANVAS_COLUMNS, CANVAS_ROWS / 3],
+        startColumn: 0,
+        startRow: 0,
+      },
+      {
+        grid: [CANVAS_COLUMNS, CANVAS_ROWS / 3],
+        startColumn: 0,
+        startRow: CANVAS_ROWS / 3,
+      },
+      {
+        grid: [CANVAS_COLUMNS, CANVAS_ROWS / 3],
+        startColumn: 0,
+        startRow: (CANVAS_ROWS / 3) * 2,
+      },
+    ],
+    canvasHeight: (defaultHeight: number) => defaultHeight * 3,
+  },
+
+  // ==========================
+  // ||    1    ||           ||
+  // =============     3     ||
+  // ||    2    ||           ||
+  // ==========================
+  {
+    positions: [
+      {
+        grid: [CANVAS_COLUMNS / 2.5, CANVAS_ROWS / 2],
+        startColumn: 0,
+        startRow: 0,
+      },
+      {
+        grid: [CANVAS_COLUMNS / 2.5, CANVAS_ROWS / 2],
+        startColumn: 0,
+        startRow: CANVAS_COLUMNS / 2,
+      },
+      {
+        grid: [CANVAS_COLUMNS - CANVAS_COLUMNS / 2.5, CANVAS_ROWS],
+        startColumn: CANVAS_COLUMNS / 2.5,
+        startRow: 0,
+      },
+    ],
+    canvasHeight: (defaultHeight: number) => defaultHeight * 3,
+    canvasWidth: (defaultWidth: number) => defaultWidth * 4,
+  },
+
+  // ==========================
+  // ||    1    ||     3     ||
+  // ========================||
+  // ||    2    ||     4     ||
+  // ==========================
+  {
+    positions: [
+      {
+        grid: [CANVAS_COLUMNS / 2, CANVAS_ROWS / 2],
+        startColumn: 0,
+        startRow: 0,
+      },
+      {
+        grid: [CANVAS_COLUMNS / 2, CANVAS_ROWS / 2],
+        startColumn: 0,
+        startRow: CANVAS_COLUMNS / 2,
+      },
+      {
+        grid: [CANVAS_COLUMNS / 2, CANVAS_ROWS / 2],
+        startColumn: CANVAS_COLUMNS / 2,
+        startRow: 0,
+      },
+      {
+        grid: [CANVAS_COLUMNS / 2, CANVAS_ROWS / 2],
+        startColumn: CANVAS_COLUMNS / 2,
+        startRow: CANVAS_ROWS / 2,
+      },
+    ],
+    canvasHeight: (defaultHeight: number) => defaultHeight * 2,
+    canvasWidth: (defaultWidth: number) => defaultWidth * 2,
+  },
+];
+
 const ScreenshotButton = () => {
   const { i18n } = useVideoProps();
   const { videoEl } = useVideo();
-  const [images, setImages] = useState<string[]>([]);
+  const [images, setImages] = useState<ScreenshotImage[]>([]);
   const [imageUrl, setImageUrl] = useState<string>("");
+  const [currentLayout, setCurrentLayout] = useState(LAYOUTS[0]);
 
   const deleteImage = (imageIndex: number) => () => {
     setImages(images.filter((_, index) => index !== imageIndex));
@@ -36,8 +176,13 @@ const ScreenshotButton = () => {
   };
 
   const snapshot = () => {
-    if (images?.length >= 2) {
-      toast.error("You can only screenshot 2 frames");
+    if (
+      images?.length >= MAX_IMAGES ||
+      images?.length >= currentLayout.positions.length
+    ) {
+      toast.error(
+        `You can only screenshot ${currentLayout.positions.length} frames, please choose another layout`
+      );
 
       return;
     }
@@ -57,7 +202,10 @@ const ScreenshotButton = () => {
     canvas.toBlob((blob) => {
       const url = URL.createObjectURL(blob);
 
-      setImages([...images, url]);
+      setImages([
+        ...images,
+        { imageUrl: url, screenshotTime: videoEl.currentTime },
+      ]);
     });
   };
 
@@ -68,17 +216,18 @@ const ScreenshotButton = () => {
   };
 
   useEffect(() => {
-    if (!images?.length || images?.length > 2) return;
+    if (images?.length > MAX_IMAGES) return;
 
     (async () => {
       const canvas = document.createElement("canvas");
       const ctx = canvas.getContext("2d");
 
-      canvas.width = IMAGE_WIDTH;
-      canvas.height = IMAGE_HEIGHT * images.length;
+      canvas.width = currentLayout?.canvasWidth?.(IMAGE_WIDTH) || IMAGE_WIDTH;
+      canvas.height =
+        currentLayout?.canvasHeight?.(IMAGE_HEIGHT) || IMAGE_HEIGHT;
 
       const loadedImages = await Promise.all(
-        images.map(async (imageUrl) => {
+        images.map(async ({ imageUrl }) => {
           const img = new Image();
           img.src = imageUrl;
           await new Promise(
@@ -91,8 +240,45 @@ const ScreenshotButton = () => {
         })
       );
 
-      loadedImages.forEach((image, index) => {
-        ctx.drawImage(image, 0, IMAGE_HEIGHT * index);
+      currentLayout.positions.forEach((position, index) => {
+        const image = loadedImages[index];
+
+        const { grid, startColumn, startRow } = position;
+
+        const [column, row] = grid;
+
+        const canvasWidthPerColumn = canvas.width / CANVAS_COLUMNS;
+        const canvasHeightPerRow = canvas.height / CANVAS_ROWS;
+
+        const x = canvasWidthPerColumn * startColumn;
+        const y = canvasHeightPerRow * startRow;
+
+        const imageCanvasWidth = canvasWidthPerColumn * column;
+        const imageCanvasHeight = canvasHeightPerRow * row;
+
+        if (!image) {
+          ctx.fillStyle = "#18191a";
+
+          ctx.fillRect(x, y, imageCanvasWidth, imageCanvasHeight);
+
+          ctx.fillStyle = "white";
+          ctx.font = `${imageCanvasWidth / 10}px Arial`;
+
+          const textString = (index + 1).toString();
+          const textWidth = ctx.measureText(textString).width;
+
+          ctx.fillText(
+            textString,
+            x + imageCanvasWidth / 2 - textWidth / 2,
+            y + imageCanvasHeight / 2
+          );
+
+          ctx.lineWidth = 8;
+          ctx.strokeStyle = "white";
+          ctx.strokeRect(x, y, imageCanvasWidth, imageCanvasHeight);
+        } else {
+          drawImageProp(ctx, image, x, y, imageCanvasWidth, imageCanvasHeight);
+        }
       });
 
       canvas.toBlob((blob) => {
@@ -100,46 +286,67 @@ const ScreenshotButton = () => {
         setImageUrl(url);
       });
     })();
-  }, [images, videoEl]);
+  }, [currentLayout, images, videoEl]);
 
   return (
     <React.Fragment>
       <Modal
         reference={
-          <ControlButton tooltip={i18n.controls.screenshot} onClick={snapshot}>
+          <ControlButton tooltip={i18n.controls.screenshot}>
             <AiOutlineCamera className="w-6 h-6" />
           </ControlButton>
         }
+        className="md:w-2/3 w-11/12"
         portalSelector=".netplayer-container"
       >
-        <div className="flex gap-20 h-96">
-          <div className="w-80 max-h-96">
+        <div className="relative">
+          <div className="flex items-center justify-center w-full overflow-auto min-h-[18rem] max-h-[20rem]">
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
-              className="w-full object-cover"
+              className="max-w-[30rem] max-h-[20rem] object-cover"
               alt="screenshot"
               src={imageUrl}
             />
           </div>
 
-          <div className="grow flex h-full flex-col justify-between">
-            <div className="space-y-2 w-full">
+          <div className="my-8 flex h-full flex-col justify-between">
+            <div className="flex items-center space-x-4 mb-8">
+              {LAYOUTS.map((layout, index) => (
+                <div
+                  className="cursor-pointer w-32 h-24"
+                  key={index}
+                  onClick={() => setCurrentLayout(layout)}
+                >
+                  <CanvasLayout layout={layout} />
+                </div>
+              ))}
+            </div>
+
+            <div className="space-y-4 w-full">
+              {!images?.length && (
+                <p className="text-center text-2xl">
+                  Click screenshot button to take a snapshot of the video
+                </p>
+              )}
+
               {images.map((image, index) => (
                 <div
-                  key={image}
+                  key={image.imageUrl}
                   className="w-full flex items-center justify-between"
                 >
                   <div className="flex items-center gap-4">
                     <p className="text-2xl">{index + 1}</p>
 
-                    <div className="w-24 aspect-w-16 aspect-h-9">
+                    <div className="grow shrink-0 w-32 aspect-w-[25] aspect-h-9">
                       {/* eslint-disable-next-line @next/next/no-img-element */}
                       <img
-                        src={image}
+                        src={image.imageUrl}
                         alt="screenshot"
                         className="object-cover"
                       />
                     </div>
+
+                    <p>{parseTime(image.screenshotTime)}</p>
                   </div>
 
                   <div className="flex itemes-center">
@@ -172,7 +379,7 @@ const ScreenshotButton = () => {
               ))}
             </div>
 
-            <div className="mt-8">
+            <div className="mt-8 px-8 py-6 bg-background-700 sticky bottom-0">
               <ProgressSlider />
 
               <div className="flex items-center justify-between mt-4">
@@ -183,6 +390,10 @@ const ScreenshotButton = () => {
                 </div>
 
                 <div className="flex items-center gap-2">
+                  <a href={imageUrl} target="_blank" rel="noreferrer">
+                    <MdOutlinePreview className="w-6 h-6 cursor-pointer" />
+                  </a>
+
                   <AiOutlineCamera
                     className="w-6 h-6 cursor-pointer"
                     onClick={snapshot}
@@ -200,6 +411,48 @@ const ScreenshotButton = () => {
       </Modal>
     </React.Fragment>
   );
+};
+
+const CanvasLayout: React.FC<{ layout: ImageLayout }> = ({ layout }) => {
+  const canvasRef = useRef<HTMLCanvasElement>();
+
+  useEffect(() => {
+    (async () => {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
+
+      canvas.width =
+        layout?.canvasWidth?.(canvas.offsetWidth) || canvas.offsetWidth;
+      canvas.height =
+        layout?.canvasHeight?.(canvas.offsetHeight) || canvas.offsetHeight;
+
+      layout.positions.forEach((position, index) => {
+        const { grid, startColumn, startRow } = position;
+
+        const [column, row] = grid;
+
+        const canvasWidthPerColumn = canvas.width / CANVAS_COLUMNS;
+        const canvasHeightPerRow = canvas.height / CANVAS_ROWS;
+
+        const x = canvasWidthPerColumn * startColumn;
+        const y = canvasHeightPerRow * startRow;
+
+        const imageCanvasWidth = canvasWidthPerColumn * column;
+        const imageCanvasHeight = canvasHeightPerRow * row;
+
+        ctx.fillStyle = "#18191a";
+
+        ctx.fillRect(x, y, imageCanvasWidth, imageCanvasHeight);
+
+        ctx.globalCompositeOperation = "source-over";
+        ctx.lineWidth = 4;
+        ctx.strokeStyle = "white";
+        ctx.strokeRect(x, y, imageCanvasWidth, imageCanvasHeight);
+      });
+    })();
+  }, [layout]);
+
+  return <canvas className="w-full h-full" ref={canvasRef}></canvas>;
 };
 
 export default ScreenshotButton;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -514,3 +514,70 @@ export const array_move = <T>(
   clonedArr.splice(new_index, 0, clonedArr.splice(old_index, 1)[0]);
   return clonedArr;
 };
+
+/**
+ * By Ken Fyrstenberg Nilsen
+ *
+ * drawImageProp(context, image [, x, y, width, height [,offsetX, offsetY]])
+ *
+ * If image and context are only arguments rectangle will equal canvas
+ */
+export function drawImageProp(
+  ctx: CanvasRenderingContext2D,
+  img: HTMLImageElement,
+  x?: number,
+  y?: number,
+  w?: number,
+  h?: number,
+  offsetX?: number,
+  offsetY?: number
+) {
+  if (arguments.length === 2) {
+    x = y = 0;
+    w = ctx.canvas.width;
+    h = ctx.canvas.height;
+  }
+
+  // default offset is center
+  offsetX = typeof offsetX === "number" ? offsetX : 0.5;
+  offsetY = typeof offsetY === "number" ? offsetY : 0.5;
+
+  // keep bounds [0.0, 1.0]
+  if (offsetX < 0) offsetX = 0;
+  if (offsetY < 0) offsetY = 0;
+  if (offsetX > 1) offsetX = 1;
+  if (offsetY > 1) offsetY = 1;
+
+  let iw = img.width,
+    ih = img.height,
+    r = Math.min(w / iw, h / ih),
+    nw = iw * r, // new prop. width
+    nh = ih * r, // new prop. height
+    cx = 0,
+    cy = 0,
+    cw = 0,
+    ch = 0,
+    ar = 1;
+
+  // decide which gap to fill
+  if (nw < w) ar = w / nw;
+  if (Math.abs(ar - 1) < 1e-14 && nh < h) ar = h / nh; // updated
+  nw *= ar;
+  nh *= ar;
+
+  // calc source rectangle
+  cw = iw / (nw / w);
+  ch = ih / (nh / h);
+
+  cx = (iw - cw) * offsetX;
+  cy = (ih - ch) * offsetY;
+
+  // make sure source rectangle is valid
+  if (cx < 0) cx = 0;
+  if (cy < 0) cy = 0;
+  if (cw > iw) cw = iw;
+  if (ch > ih) ch = ih;
+
+  // fill image in dest. rectangle
+  ctx.drawImage(img, cx, cy, cw, ch, x, y, w, h);
+}
