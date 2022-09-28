2523c0b9f661caec5b5e01eed78b8ef3d3d52f50
hoangvu12 <hggaming91@gmail.com>
2022-09-28 08:45:01 +0700

    feat(AnimeHomePage): 🎸 actual anime recently updated

---
+++ src/hooks/useRecentlyUpdated.ts
@@ -0,0 +1,34 @@
+import { AiringSort } from "@/types/anilist";
+import { removeArrayOfObjectDup } from "@/utils";
+import dayjs from "dayjs";
+import { useMemo } from "react";
+import { isMobile } from "react-device-detect";
+import useAiringSchedules from "./useAiringSchedules";
+
+const useRecentlyUpdated = () => {
+  const { data, isLoading, ...rest } = useAiringSchedules({
+    airingAt_greater: 0,
+    airingAt_lesser: dayjs().unix() - 1000,
+    perPage: isMobile ? 5 : 15,
+    sort: [AiringSort.Time_desc],
+  });
+
+  const mediaList = useMemo(() => {
+    if (isLoading) return null;
+
+    return removeArrayOfObjectDup(
+      data?.map((schedule) => schedule.media) || [],
+      "id"
+    );
+  }, [data, isLoading]);
+
+  console.log(mediaList);
+
+  return {
+    data: mediaList,
+    isLoading,
+    ...rest,
+  };
+};
+
+export default useRecentlyUpdated;
