57d50db917df937fa1681c0896bf084a4bebdc7c
hoangvu12 <hggaming91@gmail.com>
2022-10-03 11:42:31 +0700

    feat: 🎸 change media cover aspect ratio

--- src/components/shared/PlainCard.tsx
+++ src/components/shared/PlainCard.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const PlainCard: React.FC<ImageProps> = (props) => {
   return (
-    <div className="relative aspect-w-9 aspect-h-16">
+    <div className="relative aspect-w-2 aspect-h-3">
       {/* eslint-disable-next-line jsx-a11y/alt-text */}
       <Image layout="fill" objectFit="cover" {...props} />
     </div>
