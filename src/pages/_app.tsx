c14416b37d22684a9b76a95cf34de8a620d6f861
hoangvu12 <hggaming91@gmail.com>
2022-10-22 19:36:15 +0700

    feat: 🎸 add popunder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -80,6 +80,73 @@ function App({ Component, pageProps, router, err }: WorkaroundAppProps) {
         `}
       </Script>
 
+      <Script id="popunder" strategy="afterInteractive">
+        {`
+          function Set_Cookie(a, b, c, e, f, g) {
+            var d = new Date();
+            d.setTime(d.getTime());
+            d = new Date(d.getTime() + c);
+            document.cookie =
+              a +
+              "=" +
+              escape(b) +
+              (c ? ";expires=" + d.toGMTString() : "") +
+              (e ? ";path=" + e : "") +
+              (f ? ";domain=" + f : "") +
+              (g ? ";secure" : "");
+          }
+          
+          function Get_Cookie(a) {
+            var b = document.cookie.indexOf(a + "="),
+              c = b + a.length + 1;
+            if ((!b && a != document.cookie.substring(0, a.length)) || -1 == b)
+              return null;
+            a = document.cookie.indexOf(";", c);
+            -1 == a && (a = document.cookie.length);
+            return unescape(document.cookie.substring(c, a));
+          }
+          
+          function Delete_Cookie(a, b, c) {
+            Get_Cookie(a) &&
+              (document.cookie =
+                a +
+                "=" +
+                (b ? ";path=" + b : "") +
+                (c ? ";domain=" + c : "") +
+                ";expires=Mon, 11-November-2020 00:00:01 GMT");
+          }
+          
+          function popunder() {
+            if (Get_Cookie("cucre") || Get_Cookie("sb-access-token")) return;
+
+            Set_Cookie("cucre", "cucre Popunder", "1", "/", "", "")
+            
+            const pop = window.open(
+              "https://bg4nxu2u5t.com/ERT/ERT.php?c=1944171",
+              "windowcucre"
+            );
+
+            pop.blur();
+
+            window.focus()
+          }
+          
+          function addEvent(a, b, c) {
+            a.attachEvent
+              ? a.attachEvent("on" + b, c)
+              : a.addEventListener
+              ? a.addEventListener(b, c, !0)
+              : (a["on" + b] = c);
+          }
+          
+          addEvent(window, "load", function () {
+            addEvent(document.body, "click", function () {
+              popunder();
+            });
+          });        
+        `}
+      </Script>
+
       <Script
         strategy="afterInteractive"
         data-cfasync="false"
