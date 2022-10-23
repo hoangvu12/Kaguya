cb025e83db5d87f2938e88c1df3f079ad60e6600
hoangvu12 <hggaming91@gmail.com>
2022-10-23 17:18:00 +0700

    fix: 🐛 popunder expire time

--- src/lib/ads.ts
+++ src/lib/ads.ts
@@ -1,12 +1,13 @@
-const BANNER_CODES = ["1944246", "1944247"];
+import nookies from "nookies";
+
+const BANNER_CODES = ["1944246", "1944247", "1944543", "1944544", "1944545"];
+const POPUNDER_COOKIE = "kaguya_popunder";
 
 export const initBanners = () => {
   const initBanner = (code: string) => {
     const scriptId = `__clb-${code}`;
 
     const existingScript = document.getElementById(scriptId);
-
-    console.log("init banner", code);
 
     if (existingScript) {
       existingScript.remove();
@@ -24,4 +25,29 @@ export const initBanners = () => {
   };
 
   BANNER_CODES.forEach((code) => initBanner(code));
+};
+
+export const initPopunder = () => {
+  window.addEventListener("click", () => {
+    const cookies = nookies.get(null, "");
+
+    if (cookies[POPUNDER_COOKIE]) {
+      return;
+    }
+
+    const pop = window.open(
+      "https://bg4nxu2u5t.com/ERT/ERT.php?c=1944171",
+      "kaguya_popunder"
+    );
+
+    pop.blur();
+
+    window.focus();
+
+    nookies.set(null, POPUNDER_COOKIE, "1", {
+      // 6 hours
+      maxAge: 6 * 60 * 60,
+      path: "/",
+    });
+  });
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import BaseLayout from "@/components/layouts/BaseLayout";
+import { AppErrorFallback } from "@/components/shared/AppErrorFallback";
+import { AuthContextProvider } from "@/contexts/AuthContext";
+import GlobalPlayerContextProvider from "@/contexts/GlobalPlayerContext";
 import { SubscriptionContextProvider } from "@/contexts/SubscriptionContext";
+import { initBanners, initPopunder } from "@/lib/ads";
 import { GA_TRACKING_ID, pageview } from "@/lib/gtag";
 import "@/styles/index.css";
+import * as Sentry from "@sentry/nextjs";
 import { appWithTranslation } from "next-i18next";
 import nextI18nextConfig from "next-i18next.config";
 import { AppProps } from "next/app";
@@ -9,18 +14,11 @@ import Router from "next/router";
 import Script from "next/script";
 import NProgress from "nprogress";
 import React, { useEffect, useState } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
-import * as Sentry from "@sentry/nextjs";
-import { ErrorBoundary } from "react-error-boundary";
-import { AppErrorFallback } from "@/components/shared/AppErrorFallback";
-import { supabaseClient } from "@supabase/auth-helpers-nextjs";
-import { UserProvider } from "@supabase/auth-helpers-react";
-import GlobalPlayerContextProvider from "@/contexts/GlobalPlayerContext";
-import AuthContext, { AuthContextProvider } from "@/contexts/AuthContext";
-import { initBanners } from "@/lib/ads";
 
 Router.events.on("routeChangeStart", NProgress.start);
 Router.events.on("routeChangeComplete", NProgress.done);
@@ -52,6 +50,7 @@ function App({ Component, pageProps, router, err }: WorkaroundAppProps) {
     };
 
     initBanners();
+    initPopunder();
 
     router.events.on("routeChangeComplete", handleRouteChange);
 
@@ -84,91 +83,6 @@ function App({ Component, pageProps, router, err }: WorkaroundAppProps) {
             });
         `}
       </Script>
-
-      <Script id="popunder" strategy="afterInteractive">
-        {`
-          function Set_Cookie(a, b, c, e, f, g) {
-            var d = new Date();
-            d.setTime(d.getTime());
-            d = new Date(d.getTime() + c);
-            document.cookie =
-              a +
-              "=" +
-              escape(b) +
-              (c ? ";expires=" + d.toGMTString() : "") +
-              (e ? ";path=" + e : "") +
-              (f ? ";domain=" + f : "") +
-              (g ? ";secure" : "");
-          }
-          
-          function Get_Cookie(a) {
-            var b = document.cookie.indexOf(a + "="),
-              c = b + a.length + 1;
-            if ((!b && a != document.cookie.substring(0, a.length)) || -1 == b)
-              return null;
-            a = document.cookie.indexOf(";", c);
-            -1 == a && (a = document.cookie.length);
-            return unescape(document.cookie.substring(c, a));
-          }
-          
-          function Delete_Cookie(a, b, c) {
-            Get_Cookie(a) &&
-              (document.cookie =
-                a +
-                "=" +
-                (b ? ";path=" + b : "") +
-                (c ? ";domain=" + c : "") +
-                ";expires=Mon, 11-November-2020 00:00:01 GMT");
-          }
-          
-          function popunder() {
-            if (Get_Cookie("cucre") || Get_Cookie("sb-access-token")) return;
-
-            Set_Cookie("cucre", "cucre Popunder", "1", "/", "", "")
-            
-            const pop = window.open(
-              "https://bg4nxu2u5t.com/ERT/ERT.php?c=1944171",
-              "windowcucre"
-            );
-
-            pop.blur();
-
-            window.focus()
-          }
-          
-          function addEvent(a, b, c) {
-            a.attachEvent
-              ? a.attachEvent("on" + b, c)
-              : a.addEventListener
-              ? a.addEventListener(b, c, !0)
-              : (a["on" + b] = c);
-          }
-          
-          addEvent(window, "load", function () {
-            addEvent(document.body, "click", function () {
-              popunder();
-            });
-          });        
-        `}
-      </Script>
-
-      {/* <Script
-        strategy="afterInteractive"
-        data-cfasync="false"
-        type="text/javascript"
-        src="https://ssqyuvavse.com/lv/esnk/1944246/code.js"
-        async
-        id="__clb-1944246"
-      />
-
-      <Script
-        strategy="afterInteractive"
-        data-cfasync="false"
-        type="text/javascript"
-        src="https://ssqyuvavse.com/lv/esnk/1944247/code.js"
-        async
-        id="__clb-1944247"
-      /> */}
 
       <ToastContainer
         position="bottom-left"
