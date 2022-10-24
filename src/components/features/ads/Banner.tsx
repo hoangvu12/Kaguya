486af2f0422dab778ed0068312e226c4e09c37cc
hoangvu12 <hggaming91@gmail.com>
2022-10-24 22:02:50 +0700

    fix(Banner): 🐛 hide the banner if it is closed

--- src/components/features/ads/Banner.tsx
+++ src/components/features/ads/Banner.tsx
@@ -1,37 +1,20 @@
 import CircleButton from "@/components/shared/CircleButton";
 import classNames from "classnames";
-import { useRouter } from "next/router";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
-
-const MAX_SHOW_TIME = 2;
 
 const Banner = () => {
   const [isShow, setIsShow] = useState(true);
-  const count = useRef(0);
-  const router = useRouter();
 
   const handleClose = () => {
     setIsShow(false);
   };
 
-  useEffect(() => {
-    const listener = () => {
-      setIsShow(true);
-
-      count.current++;
-    };
-
-    router.events.on("routeChangeComplete", listener);
-  }, [router]);
-
-  const shouldShow = isShow && count.current < MAX_SHOW_TIME;
-
   return (
     <div
       className={classNames(
         "z-[9999] banner-ads fixed bottom-4 left-1/2 -translate-x-1/2",
-        !shouldShow && "hidden"
+        !isShow && "hidden"
       )}
     >
       <CircleButton
@@ -45,4 +28,4 @@ const Banner = () => {
   );
 };
 
-export default Banner;
+export default React.memo(Banner);
