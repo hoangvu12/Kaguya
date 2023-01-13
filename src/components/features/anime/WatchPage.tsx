67ab016c7e2023245a82ac2633be12b5920e5615
hoangvu12 <hggaming91@gmail.com>
2023-01-13 18:42:53 +0700

    feat(WatchPage): 🎸 fetch media data with isr

---
+++ src/components/features/anime/WatchPage.tsx
@@ -0,0 +1,454 @@
+import { WatchPlayerProps } from "@/components/features/anime/WatchPlayer";
+import Button from "@/components/shared/Button";
+import Description from "@/components/shared/Description";
+import Head from "@/components/shared/Head";
+import Loading from "@/components/shared/Loading";
+import Portal from "@/components/shared/Portal";
+import { useGlobalPlayer } from "@/contexts/GlobalPlayerContext";
+import useDevice from "@/hooks/useDevice";
+import useEventListener from "@/hooks/useEventListener";
+import { useFetchSource } from "@/hooks/useFetchSource";
+import useSavedWatched from "@/hooks/useSavedWatched";
+import useSaveWatched from "@/hooks/useSaveWatched";
+import { Episode } from "@/types";
+import { Media } from "@/types/anilist";
+import { parseNumberFromString } from "@/utils";
+import { getDescription, getTitle, sortMediaUnit } from "@/utils/data";
+import { NextPage } from "next";
+import { useTranslation } from "next-i18next";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+
+const WatchPlayer = dynamic(
+  () => import("@/components/features/anime/WatchPlayer"),
+  {
+    ssr: false,
+  }
+);
+
+const blankVideo = [
+  {
+    file: "https://cdn.plyr.io/static/blank.mp4",
+  },
+];
+
+const ForwardRefPlayer = React.memo(
+  React.forwardRef<HTMLVideoElement, WatchPlayerProps>((props, ref) => (
+    <WatchPlayer {...props} videoRef={ref} />
+  ))
+);
+
+ForwardRefPlayer.displayName = "ForwardRefPlayer";
+
+interface WatchPageProps {
+  episodes: Episode[];
+  media: Media;
+}
+
+const WatchPage: NextPage<WatchPageProps> = ({ episodes, media: anime }) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const router = useRouter();
+  const { isMobile } = useDevice();
+  const [showInfoOverlay, setShowInfoOverlay] = useState(false);
+  const [showWatchedOverlay, setShowWatchedOverlay] = useState(false);
+  const [declinedRewatch, setDeclinedRewatch] = useState(false);
+
+  const showInfoTimeout = useRef<NodeJS.Timeout>(null);
+  const saveWatchedInterval = useRef<NodeJS.Timer>(null);
+  const saveWatchedMutation = useSaveWatched();
+  const { t } = useTranslation("anime_watch");
+
+  const { params } = router.query;
+
+  useEventListener("visibilitychange", () => {
+    if (isMobile) return;
+
+    if (showInfoTimeout.current) {
+      clearTimeout(showInfoTimeout.current);
+    }
+
+    if (!document.hidden) return;
+
+    showInfoTimeout.current = setTimeout(() => {
+      setShowInfoOverlay(true);
+    }, 5000);
+  });
+
+  const sortedEpisodes = useMemo(
+    () => sortMediaUnit(episodes || []),
+    [episodes]
+  );
+
+  const [
+    animeId,
+    sourceId = sortedEpisodes[0].sourceId,
+    episodeId = sortedEpisodes[0].sourceEpisodeId,
+  ] = params as string[];
+
+  const {
+    data: watchedEpisodeData,
+    isLoading: isSavedDataLoading,
+    isError: isSavedDataError,
+    refetch: refetchWatchedData,
+  } = useSavedWatched(Number(animeId));
+
+  const watchedEpisode = useMemo(
+    () =>
+      isSavedDataError
+        ? null
+        : sortedEpisodes.find(
+            (episode) =>
+              episode.sourceEpisodeId ===
+              watchedEpisodeData?.episode?.sourceEpisodeId
+          ),
+    [
+      isSavedDataError,
+      sortedEpisodes,
+      watchedEpisodeData?.episode?.sourceEpisodeId,
+    ]
+  );
+
+  const sourceEpisodes = useMemo(
+    () => episodes.filter((episode) => episode.sourceId === sourceId),
+    [episodes, sourceId]
+  );
+
+  const currentEpisode = useMemo(
+    () =>
+      sourceEpisodes.find((episode) => episode.sourceEpisodeId === episodeId),
+    [sourceEpisodes, episodeId]
+  );
+
+  const sectionEpisodes = useMemo(
+    () =>
+      sourceEpisodes.filter(
+        (episode) => episode.section === currentEpisode.section
+      ),
+    [currentEpisode.section, sourceEpisodes]
+  );
+
+  const currentSectionEpisodeIndex = useMemo(
+    () =>
+      sectionEpisodes.findIndex(
+        (episode) => episode.sourceEpisodeId === episodeId
+      ),
+    [episodeId, sectionEpisodes]
+  );
+
+  const currentEpisodeIndex = useMemo(
+    () =>
+      sourceEpisodes.findIndex(
+        (episode) => episode.sourceEpisodeId === episodeId
+      ),
+    [episodeId, sourceEpisodes]
+  );
+
+  const nextEpisode = useMemo(
+    () =>
+      sectionEpisodes[currentSectionEpisodeIndex + 1] ||
+      sourceEpisodes[currentEpisodeIndex + 1],
+    [
+      currentEpisodeIndex,
+      currentSectionEpisodeIndex,
+      sectionEpisodes,
+      sourceEpisodes,
+    ]
+  );
+
+  const handleNavigateEpisode = useCallback(
+    (episode: Episode) => {
+      if (!episode) return;
+
+      router.replace(
+        `/anime/watch/${animeId}/${episode.sourceId}/${episode.sourceEpisodeId}`,
+        null,
+        {
+          shallow: true,
+        }
+      );
+    },
+    [animeId, router]
+  );
+
+  const { data, isLoading, isError, error } = useFetchSource(
+    currentEpisode,
+    nextEpisode
+  );
+
+  // Show watched overlay
+  useEffect(() => {
+    if (!currentEpisode.sourceEpisodeId) return;
+
+    if (
+      !watchedEpisode ||
+      isSavedDataLoading ||
+      isSavedDataError ||
+      declinedRewatch
+    )
+      return;
+
+    if (currentEpisode.sourceEpisodeId === watchedEpisode?.sourceEpisodeId) {
+      setDeclinedRewatch(true);
+
+      return;
+    }
+
+    setShowWatchedOverlay(true);
+  }, [
+    currentEpisode.sourceEpisodeId,
+    declinedRewatch,
+    isSavedDataError,
+    isSavedDataLoading,
+    watchedEpisode,
+  ]);
+
+  useEffect(() => {
+    const videoEl = videoRef.current;
+
+    if (!videoEl) return;
+
+    const handleSaveTime = () => {
+      if (saveWatchedInterval.current) {
+        clearInterval(saveWatchedInterval.current);
+      }
+      saveWatchedInterval.current = setInterval(() => {
+        saveWatchedMutation.mutate({
+          media_id: Number(animeId),
+          episode_id: `${currentEpisode.sourceId}-${currentEpisode.sourceEpisodeId}`,
+          watched_time: videoRef.current?.currentTime,
+          episode_number: parseNumberFromString(currentEpisode?.name, 0),
+        });
+      }, 30000);
+    };
+
+    videoEl.addEventListener("canplay", handleSaveTime);
+
+    return () => {
+      clearInterval(saveWatchedInterval.current);
+      videoEl.removeEventListener("canplay", handleSaveTime);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [animeId, currentEpisode, videoRef.current]);
+
+  useEffect(() => {
+    const videoEl = videoRef.current;
+
+    if (!videoEl) return;
+    if (isSavedDataLoading) return;
+    if (!watchedEpisodeData?.watchedTime) return;
+    if (!currentEpisode?.name) return;
+
+    const currentEpisodeNumber = parseNumberFromString(currentEpisode.name, 0);
+
+    if (currentEpisodeNumber !== watchedEpisodeData.episodeNumber) return;
+
+    const handleVideoPlay = () => {
+      videoEl.currentTime = watchedEpisodeData.watchedTime;
+
+      videoEl.removeEventListener("canplay", handleVideoPlay);
+      videoEl.removeEventListener("timeupdate", handleVideoPlay);
+    };
+
+    // Only set the video time if the video is ready
+    videoEl.addEventListener("canplay", handleVideoPlay);
+    // Just in case the video is already played.
+    videoEl.addEventListener("timeupdate", handleVideoPlay);
+
+    return () => {
+      videoEl.removeEventListener("canplay", handleVideoPlay);
+      videoEl.removeEventListener("timeupdate", handleVideoPlay);
+    };
+  }, [
+    watchedEpisode?.sourceEpisodeId,
+    watchedEpisodeData?.watchedTime,
+    currentEpisode?.name,
+    isSavedDataLoading,
+    watchedEpisodeData?.episodeNumber,
+  ]);
+
+  // Refetch watched data when episode changes
+  useEffect(() => {
+    refetchWatchedData();
+  }, [currentEpisode.slug, refetchWatchedData]);
+
+  const title = useMemo(
+    () => getTitle(anime, router.locale),
+    [anime, router.locale]
+  );
+  const description = useMemo(
+    () => getDescription(anime, router.locale),
+    [anime, router.locale]
+  );
+
+  const sources = useMemo(
+    () => (!data?.sources?.length ? blankVideo : data.sources),
+    [data?.sources]
+  );
+
+  const subtitles = useMemo(
+    () => (!data?.subtitles?.length ? [] : data.subtitles),
+    [data?.subtitles]
+  );
+
+  const fonts = useMemo(
+    () => (!data?.fonts?.length ? [] : data.fonts),
+    [data?.fonts]
+  );
+
+  useGlobalPlayer({
+    playerState: {
+      ref: videoRef,
+      sources,
+      subtitles,
+      fonts,
+      thumbnail: data?.thumbnail,
+      className: "object-contain w-full h-full",
+    },
+    playerProps: {
+      anime,
+      currentEpisode,
+      currentEpisodeIndex,
+      episodes: sortedEpisodes,
+      setEpisode: handleNavigateEpisode,
+      sourceId,
+      sources,
+    },
+  });
+
+  useEffect(() => {
+    if (!anime) return;
+
+    const syncDataScript = document.querySelector("#syncData");
+
+    syncDataScript.textContent = JSON.stringify({
+      title: anime.title.userPreferred,
+      aniId: Number(animeId),
+      episode: parseNumberFromString(currentEpisode.name),
+      id: animeId,
+      nextEpUrl: nextEpisode
+        ? `/anime/watch/${animeId}/${nextEpisode.sourceId}/${nextEpisode.sourceEpisodeId}`
+        : null,
+    });
+  }, [anime, animeId, currentEpisode.name, nextEpisode]);
+
+  return (
+    <React.Fragment>
+      <Head
+        title={`${title} (${currentEpisode.name}) - Kaguya`}
+        description={`${description}. Watch ${title} online for free.`}
+        image={anime.bannerImage}
+      />
+
+      {isLoading && (
+        <Portal selector=".netplayer-container">
+          <Loading />
+        </Portal>
+      )}
+
+      {isError ? (
+        <Portal selector=".netplayer-container">
+          <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 space-y-4">
+            <p className="text-4xl font-semibold text-center">｡゜(｀Д´)゜｡</p>
+            <p className="text-xl text-center">
+              Something went wrong{" "}
+              {error?.response?.data?.error &&
+                `(${error?.response?.data?.error})`}
+            </p>
+            <p className="text-lg text-center">
+              Try to refresh the page or choose another source.
+            </p>
+          </div>
+        </Portal>
+      ) : (
+        !isLoading &&
+        !data?.sources?.length && (
+          <Portal selector=".netplayer-container">
+            <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 space-y-4">
+              <p className="text-4xl font-semibold text-center">｡゜(｀Д´)゜｡</p>
+              <p className="text-xl text-center">
+                Something went wrong{" "}
+                {error?.response?.data?.error &&
+                  `(${error?.response?.data?.error})`}
+              </p>
+              <p className="text-lg text-center">
+                Try to refresh the page or choose another source.
+              </p>
+            </div>
+          </Portal>
+        )
+      )}
+
+      {showInfoOverlay && (
+        <Portal>
+          <div
+            className="fixed inset-0 z-[9999] flex items-center bg-black/70"
+            onMouseMove={() => setShowInfoOverlay(false)}
+          >
+            <div className="w-11/12 px-40">
+              <p className="mb-2 text-xl text-gray-200">{t("blur_heading")}</p>
+              <p className="mb-8 text-5xl font-semibold">
+                {title} - {currentEpisode.name}
+              </p>
+
+              <Description
+                description={description || t("common:updating") + "..."}
+                className="text-lg text-gray-300 line-clamp-6"
+              />
+            </div>
+          </div>
+        </Portal>
+      )}
+
+      {showWatchedOverlay && !declinedRewatch && (
+        <Portal selector=".netplayer-container">
+          <div
+            className="fixed inset-0 z-40 bg-black/70"
+            onClick={() => {
+              setShowWatchedOverlay(false);
+              setDeclinedRewatch(true);
+            }}
+          />
+
+          <div className="fixed left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 z-50 w-2/3 p-8 rounded-md bg-background-900">
+            <h1 className="text-4xl font-bold mb-4">
+              {t("rewatch_heading", { episodeName: watchedEpisode.name })}
+            </h1>
+            <p className="">
+              {t("rewatch_description", { episodeName: watchedEpisode.name })}
+            </p>
+            <p className="mb-4">
+              {t("rewatch_question", { episodeName: watchedEpisode.name })}
+            </p>
+            <div className="flex items-center justify-end space-x-4">
+              <Button
+                onClick={() => {
+                  setShowWatchedOverlay(false), setDeclinedRewatch(true);
+                }}
+                className="!bg-transparent hover:!bg-white/20 transition duration-300"
+              >
+                <p>{t("rewatch_no")}</p>
+              </Button>
+              <Button
+                onClick={() =>
+                  handleNavigateEpisode(watchedEpisodeData?.episode)
+                }
+                primary
+              >
+                <p>{t("rewatch_yes")}</p>
+              </Button>
+            </div>
+          </div>
+        </Portal>
+      )}
+    </React.Fragment>
+  );
+};
+
+export default WatchPage;
